f207e5e3a94da996b02953608483088f85d64e15
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-06-22 21:45:54 -0300

    feat: Impl. number and operations configuration arrays

---
+++ src/app/config/button-configuration.ts
@@ -0,0 +1,57 @@
+export const numberButtonConfiguration = [
+  {
+    id: 1,
+    label: '1',
+  },
+  {
+    id: 2,
+    label: '2',
+  },
+  {
+    id: 3,
+    label: '3',
+  },
+  {
+    id: 4,
+    label: '4',
+  },
+  {
+    id: 5,
+    label: '5',
+  },
+  {
+    id: 6,
+    label: '6',
+  },
+  {
+    id: 7,
+    label: '7',
+  },
+  {
+    id: 8,
+    label: '8',
+  },
+  {
+    id: 9,
+    label: '9',
+  },
+];
+
+export const operationsButtonConfiguration = [
+  {
+    id: 1,
+    label: '+',
+  },
+  {
+    id: 2,
+    label: '-',
+  },
+  {
+    id: 3,
+    label: 'x',
+  },
+  {
+    id: 4,
+    label: '/',
+  }
+];
