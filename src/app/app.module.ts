321d2e080c7a4c8bdecabf06852675e00e98c2da
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-06-22 21:46:43 -0300

    feat: Add header modulo to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HeaderModule } from './ui-components/header/header.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    CoreModule
+    CoreModule,
+    HeaderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
