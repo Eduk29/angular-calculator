72f3afffd9fd73c91044317d5040f3d2f3d2fd1a
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-06-22 21:44:58 -0300

    feat: Impl. header component

---
+++ src/app/ui-components/header/header.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class HeaderModule { }
