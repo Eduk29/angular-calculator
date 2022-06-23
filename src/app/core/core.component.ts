589742fa29979fc23c847c083ff479dc96013f11
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2022-06-22 21:47:22 -0300

    feat: Impl. numbers display layout in calculator

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+
+import { numberButtonConfiguration } from './../config/button-configuration';
 
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
-  styleUrls: ['./core.component.scss']
+  styleUrls: ['./core.component.scss'],
 })
 export class CoreComponent implements OnInit {
+  public buttonArray: any[] = [];
+  public displayData: string | number = 0;
+  public buttonMatrixDisplay: any [] [] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.buttonArray = numberButtonConfiguration;
+    this.buttonMatrixDisplay = this.createNumberButtonsMatrix();
   }
 
+  private createNumberButtonsMatrix(): any [] [] {
+    const buttonMatrix: any[] [] = [];
+    const numberRows = numberButtonConfiguration.length / 3;
+    const rowSize = 3;
+    let initial = 0;
+
+    for (let i = 0; i < numberRows; i++) {
+      buttonMatrix.push(this.createMatrixRow(initial, rowSize));
+      initial += 3;
+    }
+    console.log(buttonMatrix)
+    return buttonMatrix;
+  }
+
+  private createMatrixRow(initial: number, rowSize: number): any[] {
+    let buttonRow: any[] = [];
+
+    this.buttonArray.map(() => {
+      buttonRow = this.buttonArray.slice(initial, initial + rowSize);
+    });
+    return buttonRow;
+  }
 }
+
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
-
+import { ButtonModule } from './../ui-components/button/button.module';
+import { DisplayModule } from './../ui-components/display/display.module';
+import { CoreComponent } from './core.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoreComponent
+  ],
   imports: [
-    CommonModule
+    ButtonModule,
+    CommonModule,
+    DisplayModule,
+    MatCardModule
+  ],
+  exports: [
+    CoreComponent
   ]
 })
 export class CoreModule { }
